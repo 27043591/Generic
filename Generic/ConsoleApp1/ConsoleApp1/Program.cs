6b1de03ccd4a60a983baf108b56fe6894a7ef453
Maheshwar Maheshwar <27043591@student.toiohomai.ac.nz>
2019-04-05 13:34:34 +1300

    generic

---
+++ Generic/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int IntInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your number");
+            string strInput = Console.ReadLine();
+            GenericClass<int, string> compare = new GenericClass<int, string>();
+            if (compare.Check(IntInput, strInput))
+                Console.WriteLine("they are same");
+            else
+                Console.WriteLine("they are not same");
+            Console.ReadLine();
+        }
+        
+    }
+    class GenericClass<I, T>
+    {
+        public bool Check(I x, T y)
+        {
+            if (x.ToString().Equals(y.ToString()))
+                return true;
+            else
+                return false;
+        
+        }
+
+       
+    }
+  
+}
